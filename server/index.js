ae227de73904e1830464e39c359d3e07afd85320
Nikita kadiwike <ninavenikita492@gmail.com>
2023-12-06 08:48:14 +0530

    basic set up of server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app= express()
+
+app.use(express.json());
+
+const connectDB =  async ()=>{
+ const conn = await mongoose.connect(process.env.MONGODB_URI)
+ if(conn){
+    console.log("mongodb connected successfully !")
+ }
+}
+connectDB();
+const PORT = process.env.PORT || 5000;
+
+app.listen( PORT,()=>{
+console.log("server is connected successfully !")
+})
+
+
